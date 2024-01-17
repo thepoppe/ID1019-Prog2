94b36e1f0bd3b7e04732114cc9575c2fd3922cb4
thepoppe <pontus.morin@hotmail.se>
2024-01-18 00:57:31 +0100

    naming

---
+++ Task1/hello.ex
@@ -0,0 +1,5 @@
+defmodule Hello do
+  def world(name) do
+    IO.puts("Hello Elixir, this is #{name}")
+  end
+end
--- Task1/hello.exs
+++
@@ -1,5 +0,0 @@
-defmodule Hello do
-  def world(name) do
-    IO.puts("Hello Elixir, this is #{name}")
-  end
-end
