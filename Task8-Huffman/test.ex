e233b6cdea7e9a5e1b7dcfaf789e2228eea6b354
thePoppe <pontus.morin@hotmail.se>
2024-03-07 18:28:02 +0100

    bench 4 removed

--- Task8-Huffman/test.ex
+++ Task8-Huffman/test.ex
@@ -77,23 +77,7 @@ defmodule Test do
   IO.puts("Time for completion #{total}")
   IO.puts("source #{size} bytes, encoded #{e} bytes, compression #{r}")
  end
- def bench4 do
-  {:ok,text, _len, size} = read()
-  #IO.puts(text)
-  {total, {_separate, _tree, encoded, _decoded}} = :timer.tc(fn ->
-    {t1, tree} =    :timer.tc(fn -> Huff.tree(text) end)
-    {t2, e_table} = :timer.tc(fn -> Encode.table(tree) end)
-    {t3, encoded} = :timer.tc(fn -> Encode.encode2(text, e_table) end)
-    {t4, _d_table} = :timer.tc(fn -> Decode.decode_table(e_table) end)
-    {t5, decoded} = :timer.tc(fn -> Decode.decode2(encoded, tree) end)
-    {[t1,t2,t3,t4,t5], tree, encoded, decoded}
-  end)
-  #tree_size = Kernel.byte_size(tree)
-  e = div(length(encoded), 8)
-  r = Float.round(e / size, 3)
-  IO.puts("Time for completion #{total}")
-  IO.puts("source #{size} bytes, encoded #{e} bytes, compression #{r}")
- end
+
 
  def read() do
   text = File.read!("kallocain.txt")
