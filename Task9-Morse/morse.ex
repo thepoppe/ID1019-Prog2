5f104f6c265c41fdb027984ec783255cbc3f7526
thePoppe <pontus.morin@hotmail.se>
2024-03-18 10:32:15 +0100

    morse code done

---
+++ Task9-Morse/morse.ex
@@ -0,0 +1,113 @@
+defmodule Morse do
+
+
+  def main do
+    msg = String.to_charlist("pontus morin")
+    encode(msg)
+    #decode('... --- ...')
+    #decode(MorseCode.base())
+  end
+
+  def encode([], _table) do [] end
+  def encode([char|rest], table) do
+     Map.get(table,char) ++ [?\s] ++ encode(rest, table)
+  end
+
+  def encode(input) do
+    table = traverse(MorseCode.tree(), [], Map.new())
+    encode(input, table)
+  end
+
+  def traverse(:nil, _, map) do map end
+  def traverse({:node, :na, long, short}, path, map) do
+    map = traverse(long, [?-|path], map)
+    traverse(short, [?.|path], map)
+  end
+
+  def traverse({:node, char, long, short}, path, map) do
+    map = Map.put(map, char, Enum.reverse(path))
+    map = traverse(long, [?-|path], map)
+    traverse(short, [?.|path], map)
+
+  end
+
+
+
+  def decode(input) do
+    tree = MorseCode.tree()
+    decode(input, tree)
+
+  end
+  def decode(input, tree) do
+    decode(input,tree,tree)
+  end
+
+  def decode([],root,root) do [] end
+  def decode([],{:node, char, _,_},_root) do [char] end
+  def decode([?-| msg],{:node, _, long, _},root) do
+    decode(msg, long, root)
+  end
+  def decode([?.| msg],{:node, _, _, short},root) do
+    decode(msg, short, root)
+  end
+  def decode([?\s| msg],{:node, char, _, _},root) do
+    [char | decode(msg, root, root)]
+  end
+end
+
+
+
+defmodule MorseCode do
+
+  # The codes that you should decode:
+
+  def base, do: '.- .-.. .-.. ..-- -.-- --- ..- .-. ..-- -... .- ... . ..-- .- .-. . ..-- -... . .-.. --- -. --. ..-- - --- ..-- ..- ...'
+
+  def rolled, do: '.... - - .--. ... ---... .----- .----- .-- .-- .-- .-.-.- -.-- --- ..- - ..- -... . .-.-.- -.-. --- -- .----- .-- .- - -.-. .... ..--.. ...- .----. -.. .--.-- ..... .---- .-- ....- .-- ----. .--.-- ..... --... --. .--.-- ..... ---.. -.-. .--.-- ..... .----'
+
+  # The decoding tree.
+  #
+  # The tree has the structure  {:node, char, long, short} | :nil
+  #
+
+  def tree do
+    {:node, :na,
+      {:node, 116,
+        {:node, 109,
+          {:node, 111,
+            {:node, :na, {:node, 48, nil, nil}, {:node, 57, nil, nil}},
+            {:node, :na, nil, {:node, 56, nil, {:node, 58, nil, nil}}}},
+          {:node, 103,
+            {:node, 113, nil, nil},
+            {:node, 122,
+              {:node, :na, {:node, 44, nil, nil}, nil},
+              {:node, 55, nil, nil}}}},
+        {:node, 110,
+          {:node, 107, {:node, 121, nil, nil}, {:node, 99, nil, nil}},
+          {:node, 100,
+            {:node, 120, nil, nil},
+            {:node, 98, nil, {:node, 54, {:node, 45, nil, nil}, nil}}}}},
+      {:node, 101,
+        {:node, 97,
+          {:node, 119,
+            {:node, 106,
+              {:node, 49, {:node, 47, nil, nil}, {:node, 61, nil, nil}},
+              nil},
+            {:node, 112,
+              {:node, :na, {:node, 37, nil, nil}, {:node, 64, nil, nil}},
+              nil}},
+          {:node, 114,
+            {:node, :na, nil, {:node, :na, {:node, 46, nil, nil}, nil}},
+            {:node, 108, nil, nil}}},
+        {:node, 105,
+          {:node, 117,
+            {:node, 32,
+              {:node, 50, nil, nil},
+              {:node, :na, nil, {:node, 63, nil, nil}}},
+            {:node, 102, nil, nil}},
+          {:node, 115,
+            {:node, 118, {:node, 51, nil, nil}, nil},
+            {:node, 104, {:node, 52, nil, nil}, {:node, 53, nil, nil}}}}}}
+  end
+
+  end
